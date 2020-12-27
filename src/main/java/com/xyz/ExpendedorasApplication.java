fa967b45553a291155eb3d72d186890f70078268
Elthon Chocano Pareja <elthonchocano@gmail.com>
2020-12-27 11:09:43 -0500

    first commit

---
+++ src/main/java/com/xyz/ExpendedorasApplication.java
@@ -0,0 +1,13 @@
+package com.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpendedorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpendedorasApplication.class, args);
+	}
+
+}
